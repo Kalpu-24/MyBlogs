1dd9912c4d7c9964cc0132de9941a50cbcdcef7a
Kalpu-24 <22amtics013@gmail.com>
2025-07-30 17:31:45 +0530

    chenges for mobile

--- script.js
+++ script.js
@@ -96,6 +96,11 @@ function UpdateHoverElements() {
     hoverElements = document.querySelectorAll('.post-card');
     const cursor = document.querySelector('.cursor');
 
+    if (window.matchMedia('(hover: none)').matches) {
+        cursor.classList.add('is-hidden');
+        return;
+    }
+    
     const onMouseMove = (e) => {
         cursor.style.left = e.clientX + 'px';
         cursor.style.top = e.clientY + 'px';
